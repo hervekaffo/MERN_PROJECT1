17ae4560d7f945e9fec427dd69fbc45a410016b3
Kaffo <kaffo80@gmail.com>
2019-08-08 13:16:24 -0500

    Little change

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,57 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/users/profile
-// @desc    Tests users route
-// @access  Public (do not need a token)
-//router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
-router.get("/", (req, res) => res.send("Profile route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//const Post = require("../../models/Post");
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  private (need a token)
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
